d246624dd56aa7752a8467296b9071f191c8fbdb
othiagomanhaes <othiagomanhaes@gmail.com>
2023-01-04 15:50:52 -0300

    Footer pronto.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,29 @@
 import '../styles/Footer.css';
+import imgLinkedIn from '../images/img-LinkedIn.png';
+import imgGitHub from '../images/img-Github.png';
+import insta from '../images/instagram.png';
+import insta2 from '../images/instagram-2.png';
 
 function Footer() {
   return (
     <footer id="footer-Foo">
-      <h2>Eu sou o footer</h2>
+      <div id="div-footer">
+        <h2 id="title-contact">Entre em contato comigo</h2>
+
+        <div className="redes-footer">
+          <a href="https://www.linkedin.com/in/thiago-manhaes/" target="_blank" rel="noreferrer">
+            <img className="img-redes" src={ imgLinkedIn } alt="ícone LinkedIn" />
+          </a>
+          <a href="https://github.com/othiagomanhaes" target="_blank" rel="noreferrer">
+            <img className="img-redes" src={ imgGitHub } alt="ícone Github" />
+          </a>
+          <a href="https://www.instagram.com/othiagomanhaes/" target="_blank" rel="noreferrer">
+            <img className="img-redes" src={ insta2 } alt="ícone Instagram" />
+          </a>
+          <p>othiagomanhaes@gmail.com</p>
+        </div>
+        <p id="msg-final">{"Criado e Desenvolvido com carinho e código por Thiago Manhães =)"}</p>
+      </div>
     </footer>
   );
 }
