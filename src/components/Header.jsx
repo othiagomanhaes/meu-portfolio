1943a3623dd2b7ae40824aa64a7d0f5e5a0d43ff
othiagomanhaes <othiagomanhaes@gmail.com>
2023-02-26 14:24:58 -0300

    Mudança no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,51 @@
-import { useEffect } from 'react';
-import anime from 'animejs/lib/anime.es.js';
+// import Typical from 'react-typical';
+import { useEffect } from "react";
+import TypeIt from "typeit";
 import '../styles/Header.css';
 import pessoaProgra from '../images/pessoaProgra.png';
 
 function Header() {
   useEffect(() => {
-    anime({
-      targets: '#section-2-header',
-      translateY: [10, 150], // from 100 to 250
-      delay: 500,
-      direction: 'alternate',
-      // rotate: '1turn',
-    });
-  }, []);
-
-
+    new TypeIt("#p-ola", {
+      speed: 80,
+      waitUntilVisible: true,
+    })
+      .type("Hllo", { delay: 300 })
+      .move(-3)
+      // .delete(1)
+      .type("e")
+      .move(null, { to: "END" })
+      .type(", world! ")
+      .break({ delay: 500 })
+      .type("I'm ")
+      .type("<span><b>Thiago</b></span>")
+      .pause(300)
+      .break({ delay: 500 })
+      .type("Geek")
+      .pause(500)
+      .delete(4)
+      .pause(500)
+      .type("Gamer")
+      .pause(500)
+      .delete(5)
+      .pause(500)
+      .type("<b>Front-End Developer</b> ")
+      // .move(-4)
+      // .type("o")
+      // .move(null, { to: "END" })
+      // .type("h of today.")
+      // .pause(500)
+      // .break({ delay: 500 })
+      // .break({ delay: 500 })
+      // .type("<em>- Will Rogers</em>")
+      .go();
+  }, [])
+  
   return (
     <header id="header-port">
       <section id="section-2-header">
         <div id="div1-section-2">
-          <p id="p-ola">Olá, eu sou</p>
-          <p id="meu-nome">Thiago Manhães</p>
-          <h4 id="title-front">Front-End Developer</h4>
+          <p id="p-ola" />
         </div>
 
         <div id="div2-section-2">
