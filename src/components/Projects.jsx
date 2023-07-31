5c6ffefb253b40c9517c9d8c32e65fd6f8fcc906
othiagomanhaes <othiagomanhaes@gmail.com>
2023-07-31 14:08:03 -0300

    Refatorando componente projetos.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,7 @@
 import '../styles/Projects.css';
 import { useEffect } from 'react';
 import ScrollReveal from 'scrollreveal';
-import trybetunes from '../images/trybetunes-video.gif';
-import trunfoHerois from '../images/trunfo-herois.gif';
-import linkWhite from '../images/link-externo-white.png';
-import imgGithub from '../images/img-Github.png'
+import projects from '../services/projects';
 
 
 function Projects() {
@@ -18,37 +15,24 @@ function Projects() {
         <div className="div-hidden-projects">
           <h3 id="title-projects">Meus Projetos</h3>
           <div id="div-projects">
-            <div className="project">
-              <img src={ trybetunes } alt="iphone" className="imgsProject"/>
-              <div className="project-text">
-                <div className="descreve-project">
-                  <h3>Trybetunes</h3>
-                  <div className="imgs-icones">
-                    <a href="https://othiagomanhaes.github.io/trybetunes/" target="_blank" rel="noreferrer"><img src={ linkWhite } alt="linkLogo" /></a>
-                    <a href="https://github.com/othiagomanhaes/trybetunes" target="_blank" rel="noreferrer"><img src={ imgGithub } alt="linkLogo" /></a>
+            { projects.map((
+              {imagem, titulo, linkAlive, imgLink, linkGithub, imgGithub, texto}, ind) => (
+              <div className="project" key={ind}>
+                <img src={ imagem } alt="iphone" className="imgsProject"/>
+                <div className="project-text">
+                  <div className="descreve-project">
+                    <h3>{titulo}</h3>
+                    <div className="imgs-icones">
+                      <a href={linkAlive} target="_blank" rel="noreferrer"><img src={ imgLink } alt="linkLogo" /></a>
+                      <a href={linkGithub} target="_blank" rel="noreferrer"><img src={ imgGithub } alt="linkLogo" /></a>
+                    </div>
                   </div>
+                  <p>{texto}</p>
                 </div>
-                <p>Uma apliacação em React com chamada a API da Apple para buscar artistas e albuns. Também se pode escutar a prévia de suas musicas e favoritá-las.</p>
-              </div>
-            </div>
-
-            <div className="project">
-              <img src={ trunfoHerois } alt="iphone" className="imgsProject"/>
-              <div className="project-text">
-                <div className="descreve-project">
-                  <h3>Tryunfo DC</h3>
-                  <div className="imgs-icones">
-                    <a href="https://othiagomanhaes.github.io/trunfo-herois-dc" target="_blank" rel="noreferrer"><img src={ linkWhite } alt="linkLogo" /></a>
-                    <a href="https://github.com/othiagomanhaes/trunfo-herois-dc" target="_blank" rel="noreferrer"><img src={ imgGithub } alt="linkLogo" /></a>
-                  </div>
-                </div>
-                  <p>Uma aplicação de estados no React com componentes de classe. Usando Forms e fazendo a replica do jogo de cartas com tema Herois DC.</p>
-              </div>
-            </div>
-
+            </div> 
+            ))}
             <div className="project">
             </div>
-
           </div>
         </div>
       </section>
---
+++ src/services/projects.js
@@ -0,0 +1,28 @@
+import trybetunes from '../images/trybetunes-video.gif';
+import linkWhite from '../images/link-externo-white.png';
+import imgGithub from '../images/img-Github.png'
+import trunfoHerois from '../images/trunfo-herois.gif';
+
+const projects = [
+  {
+    imagem: trybetunes,
+    titulo: 'Trybetunes',
+    linkAlive: 'https://othiagomanhaes.github.io/trybetunes/',
+    imgLink: linkWhite,
+    linkGithub: 'https://github.com/othiagomanhaes/trybetunes',
+    imgGithub: imgGithub,
+    texto: 'Uma apliacação em React com chamada a API da Apple para buscar artistas e albuns. Também se pode escutar a prévia de suas musicas e favoritá-las.',
+  },
+  
+  {
+    imagem: trunfoHerois,
+    titulo: 'Tryunfo DC',
+    linkAlive: 'https://othiagomanhaes.github.io/trunfo-herois-dc/',
+    imgLink: linkWhite,
+    linkGithub: 'https://github.com/othiagomanhaes/trunfo-herois-dc',
+    imgGithub: imgGithub,
+    texto: 'Uma aplicação que manipula estados no React com componentes de classe. Usando Forms e fazendo a réplica do jogo de cartas com tema Herois DC.',
+  },
+];
+
+export default projects;
