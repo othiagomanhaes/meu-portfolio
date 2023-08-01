09026341f4ee93979128899eeb4a96d59429694f
othiagomanhaes <othiagomanhaes@gmail.com>
2023-07-31 22:36:08 -0300

    Modificação nas skills e e fatoramento.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,24 +1,16 @@
 import '../styles/Skills.css';
 import { useEffect, useState } from 'react';
 import ScrollReveal from 'scrollreveal';
+import skillsArray from '../services/skills';
 
 function Skills() {
   const textDefaultTec = 'Passe o mouse por cima da habilidade ao lado para saber mais.'
   const [descriTec, setDescriTec] = useState(textDefaultTec);
 
   const showDescriSkill = (skill) => {
-    const skills = {
-      github: 'GitHub é uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git.',
-      redux: 'Redux é uma biblioteca JavaScript de código aberto para gerenciar o estado do aplicativo.',
-      git: 'Git é um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software, mas pode ser usado para registrar o histórico de edições de qualquer tipo de arquivo.',
-      react: 'O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.',
-      javascript: 'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.',
-      rtl: 'A React Testing Library é um conjunto de ferramentas voltadas para testar cada um dos componentes React que o desenvolvedor utilizar.',
-      css: 'Cascading Style Sheets é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags HTML ou ficar contido dentro das tags <style>.',
-      html: 'HTML é uma linguagem de marcação utilizada na construção de páginas na Web.',
-      jest: 'Jest é um poderoso Framework de Testes em JavaScript com um foco na simplicidade.',
-    };
-    setDescriTec(skills[skill]);
+    const skills = skillsArray.find(({ nome }) => nome === skill)
+
+    setDescriTec(skills.texto);
   }
 
   useEffect(() => {
@@ -30,84 +22,23 @@ function Skills() {
   }
 
   return (
-    <div>
+    <div className="div-skills">
       <section id="section-skills">
         <div className="div-hidden-skills">
           <h3 id="title-skills">Minhas Habilidades</h3>
           <div id="div-list">
             <ul id="list-skills">
-              <li
-                className="li-tecs"
-                onMouseOver={ () => { showDescriSkill('html'); } }
-                onMouseOut={ () => { defaulTextTec(); } }
-              >
-                <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html"/>
-                HTML
-              </li>
-              <li
-                className="li-tecs"
-                onMouseOver={ () => { showDescriSkill('css'); } }
-                onMouseOut={ () => { defaulTextTec(); } }
-              >
-                <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="css" />
-                CSS
-              </li>
-              <li
-                className="li-tecs"
-                onMouseOver={ () => { showDescriSkill('javascript'); } }
-                onMouseOut={ () => { defaulTextTec(); } }
-              >
-                <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javaScript" />
-                JavaScript
-              </li>
-              <li
-                className="li-tecs"
-                onMouseOver={ () => { showDescriSkill('react'); } }
-                onMouseOut={ () => { defaulTextTec(); } }
-              >
-                <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react" />
-                React
-              </li>
-              <li
-                className="li-tecs"
-                onMouseOver={ () => { showDescriSkill('redux'); } }
-                onMouseOut={ () => { defaulTextTec(); } }
-              >
-                <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt="redux" />
-                Redux
-              </li>
-              <li
-                className="li-tecs"
-                onMouseOver={ () => { showDescriSkill('jest'); } }
-                onMouseOut={ () => { defaulTextTec(); } }
-              >
-                <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" alt="jest" />
-                Jest
-              </li>
-              <li
-                className="li-tecs"
-                onMouseOver={ () => { showDescriSkill('rtl'); } }
-                onMouseOut={ () => { defaulTextTec(); } }
-              >
-                <img className="img-tec" src="https://testing-library.com/img/octopus-128x128.png" alt="react testing library" />
-                RTL
-              </li>
-              <li
-                className="li-tecs"
-                onMouseOver={ () => { showDescriSkill('git'); } }
-                onMouseOut={ () => { defaulTextTec(); } }
-              >
-                <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="git" />
-                Git
-              </li>
-              <li
-                className="li-tecs"
-                onMouseOver={ () => { showDescriSkill('github'); } }
-                onMouseOut={ () => { defaulTextTec(); } }
-              >
-                <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="github" />
-                Github
-              </li>
+              {skillsArray.map(({ nome, imagem }, ind) => (
+                <li
+                  className="li-tecs"
+                  onMouseOver={ () => { showDescriSkill(nome); } }
+                  onMouseOut={ () => { defaulTextTec(); } }
+                  key={ ind }
+                >
+                  <img className="img-tec" src={ imagem } alt={ nome }/>
+                  { nome }
+                </li>
+              ))}
             </ul>
             <div id="div-disc-tec">{descriTec}</div>
           </div>
@@ -118,3 +49,69 @@ function Skills() {
 }
 
 export default Skills;
+
+// </li>
+//               <li
+//                 className="li-tecs"
+//                 onMouseOver={ () => { showDescriSkill('css'); } }
+//                 onMouseOut={ () => { defaulTextTec(); } }
+//               >
+//                 <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="css" />
+//                 CSS
+//               </li>
+//               <li
+//                 className="li-tecs"
+//                 onMouseOver={ () => { showDescriSkill('javascript'); } }
+//                 onMouseOut={ () => { defaulTextTec(); } }
+//               >
+//                 <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javaScript" />
+//                 JavaScript
+//               </li>
+//               <li
+//                 className="li-tecs"
+//                 onMouseOver={ () => { showDescriSkill('react'); } }
+//                 onMouseOut={ () => { defaulTextTec(); } }
+//               >
+//                 <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react" />
+//                 React
+//               </li>
+//               <li
+//                 className="li-tecs"
+//                 onMouseOver={ () => { showDescriSkill('redux'); } }
+//                 onMouseOut={ () => { defaulTextTec(); } }
+//               >
+//                 <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt="redux" />
+//                 Redux
+//               </li>
+//               <li
+//                 className="li-tecs"
+//                 onMouseOver={ () => { showDescriSkill('jest'); } }
+//                 onMouseOut={ () => { defaulTextTec(); } }
+//               >
+//                 <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" alt="jest" />
+//                 Jest
+//               </li>
+//               <li
+//                 className="li-tecs"
+//                 onMouseOver={ () => { showDescriSkill('rtl'); } }
+//                 onMouseOut={ () => { defaulTextTec(); } }
+//               >
+//                 <img className="img-tec" src="https://testing-library.com/img/octopus-128x128.png" alt="react testing library" />
+//                 RTL
+//               </li>
+//               <li
+//                 className="li-tecs"
+//                 onMouseOver={ () => { showDescriSkill('git'); } }
+//                 onMouseOut={ () => { defaulTextTec(); } }
+//               >
+//                 <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="git" />
+//                 Git
+//               </li>
+//               <li
+//                 className="li-tecs"
+//                 onMouseOver={ () => { showDescriSkill('github'); } }
+//                 onMouseOut={ () => { defaulTextTec(); } }
+//               >
+//                 <img className="img-tec" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="github" />
+//                 Github
+//               </li>
---
+++ src/services/skills.js
@@ -0,0 +1,105 @@
+const skills = [
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+    nome:'HTML',
+    texto: 'HTML é uma linguagem de marcação utilizada na construção de páginas na Web.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+    nome:'CSS',
+    texto: 'Cascading Style Sheets é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags HTML ou ficar contido dentro das tags <style>.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    nome:'JavaScript',
+    texto: 'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    nome:'React',
+    texto: 'O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+    nome:'Redux',
+    texto: 'Redux é uma biblioteca JavaScript de código aberto para gerenciar o estado do aplicativo',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
+    nome:'Jest',
+    texto: 'Jest é um poderoso Framework de Testes em JavaScript com um foco na simplicidade.',
+  },
+
+  {
+    imagem: 'https://testing-library.com/img/octopus-128x128.png',
+    nome:'RTL',
+    texto: 'A React Testing Library é um conjunto de ferramentas voltadas para testar cada um dos componentes React que o desenvolvedor utilizar.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+    nome:'Git',
+    texto: 'Git é um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software, mas pode ser usado para registrar o histórico de edições de qualquer tipo de arquivo.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+    nome:'Github',
+    texto: 'GitHub é uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+    nome:'Docker',
+    texto: 'Docker é um conjunto de produtos de plataforma como serviço que usam virtualização de nível de sistema operacional para entregar software em pacotes chamados contêineres. ',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg',
+    nome:'Node.JS',
+    texto: 'Node.js é um software de código aberto, multiplataforma, baseado no interpretador V8 do Google e que permite a execução de códigos JavaScript fora de um navegador web.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg',
+    nome:'Express',
+    texto: 'Express.js é um framework para Node.js que fornece recursos mínimos para construção de servidores web.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg',
+    nome:'TypeScript',
+    texto: 'TypeScript é uma linguagem de programação de código aberto desenvolvida pela Microsoft.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg',
+    nome:'MySQL',
+    texto: 'O MySQL é um sistema de gerenciamento de banco de dados, que utiliza a linguagem SQL como interface.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-plain.svg',
+    nome:'Sequelize',
+    texto: 'O Sequelize é um ORM, ou seja, um mapeador objeto-relacional. Isso significa que ele permite que você crie modelos (classes) em TypeScript ou JavaScript que representam as tabelas do seu banco de dados.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
+    nome:'MongoDB',
+    texto: 'MongoDB é um software de banco de dados orientado a documentos livre, de código aberto e multiplataforma, escrito na linguagem C++.',
+  },
+
+  {
+    imagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg',
+    nome:'Mocha',
+    texto: 'O Mocha é uma estrutura de teste JavaScript rica em recursos em execução no Node.js e no navegador, tornando o teste assíncrono simples e divertido.',
+  },
+]
+
+export default skills;
