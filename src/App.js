8c20292418c45aeb21d552fcdd03deb5baff6afd
othiagomanhaes <othiagomanhaes@gmail.com>
2023-08-17 22:25:57 -0300

    Retirada de rotas do trybetunes e tryunfo.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import SkillsPage from './Pages/SkillsPage';
 import ProjectsPage from './Pages/ProjectsPage';
 import MainPage from './Pages/MainPage';
-import TrunfoDC from './Pages/TrunfoDC';
-import Trybetune from './Pages/Trybetunes';
 import './App.css';
 
 function App() {
@@ -15,8 +13,6 @@ function App() {
         <Route path="/meu-portfolio/abouteMe" component={ AboutPage } />
         <Route path="/meu-portfolio/skills" component={ SkillsPage }/>
         <Route path="/meu-portfolio/projects" component={ ProjectsPage }/>
-        <Route path="/projects/trunfo-herois-dc" component={ TrunfoDC }/>
-        <Route path="/projects/trybetunes" component={ Trybetune }/>
       </Switch>
     </div>
   );
